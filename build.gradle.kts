4a689480a4a36e2e5d550eb0e4f499da526e77f6
Aishwarya Chakravarthy <ashchakravarthy@gmail.com>
2021-08-30 17:18:27 +0000

    added dependencies lombak

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,22 @@ group = "io.humourmind"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
+	maven { url = uri("https://repo.spring.io/release") }
 	mavenCentral()
 }
 
 dependencies {
+	compileOnly("org.projectlombok:lombok")
+//	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
